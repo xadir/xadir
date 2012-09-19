56498a29c57880aef9b60470f848eb7aea6c2340
Zokol <juva.hj@gmail.com>
2012-09-20 00:21:43 +0300

    Movement grid and mouse events

--- src/second_test/createWindow.py
+++ src/second_test/createWindow.py
@@ -8,7 +8,7 @@ if not pygame.mixer: print "Warning: Audio not enabled"
 
 SIZE = int(sys.argv[1])
 
-class PyManMain:
+class xadir_main:
 	# Main class for initialization and mechanics of the game
 
 	def __init__(self, width=1200, height=720):
@@ -17,22 +17,26 @@ class PyManMain:
 		self.height = height
 		self.screen = pygame.display.set_mode((self.width, self.height))
 
-	def MainLoop(self):
-		self.LoadSprites();
+	def main_loop(self):
+		self.load_sprites()
 		while 1:
 			self.map_sprites.draw(self.screen)
 			self.player_sprites.draw(self.screen)
-                	pygame.display.flip()
+			self.grid_sprites.draw(self.screen)
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					sys.exit()
+				if event.type == pygame.MOUSEBUTTONDOWN:
+					self.click()
+			pygame.display.flip()
+			self.update_sprites()
 			time.sleep(0.05)
 
-	def LoadSprites(self):
+	def load_sprites(self):
     		"""Load the sprites that we need"""
 		w = 'w'
 		l = 'l'
-		self.map = Map([
+		self.map = background_map([
 				[w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w],
 				[w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w],
                                 [w,w,w,w,l,l,l,l,l,l,l,l,l,l,l,l,l,l,l,l],
@@ -49,71 +53,137 @@ class PyManMain:
                                 [w,w,l,l,l,l,l,l,l,l,l,l,l,l,l,l,l,l,l,l],
                                 [w,w,l,l,l,l,l,l,l,l,l,l,l,l,l,l,l,l,l,l],
 				], 20, 15)
-		self.player1 = Player([
+		self.player1 = player([
 					['b', 4, 3]
 				])
-		self.map_sprites = self.map.getSprites()
-		self.player_sprites = self.player1.getSprites()
+		self.grid_sprites = pygame.sprite.Group()
+		self.map_sprites = self.map.get_sprites()
+		self.player_sprites = self.player1.get_sprites()
 
-class Map:
+	def click(self):
+		characters = self.player1.get_characters()
+		for i in range(len(characters)):
+			char_coords = characters[i].get_coords()
+			mouse_coords = list(pygame.mouse.get_pos())
+			mouse_coords = [mouse_coords[0]/SIZE, mouse_coords[1]/SIZE]
+			print char_coords
+			print mouse_coords
+			if char_coords == mouse_coords:
+				print "Clicked on character"
+				if characters[i].is_selected():
+					characters[i].unselect()
+					self.grid_sprites = pygame.sprite.Group()
+				else:
+					characters[i].select()
+					self.movement_grid = sprite_grid(characters[i].get_movement_grid(), characters[i].get_coords())
+					self.grid_sprites = self.movement_grid.get_sprites()			
+			elif characters[i].is_selected():
+				if characters[i].is_legal_move(mouse_coords):
+					characters[i].set_coords(mouse_coords)
+					self.grid_sprites = pygame.sprite.Group()
+					characters[i].unselect
+			if char_coords != mouse_coords:
+				characters[i].unselect()
+	def update_sprites(self):
+		self.player1.update_sprites()
+		self.player_sprites = self.player1.get_sprites()
+
+class sprite_grid:
+	#print "Loading opaquegrid"
+	#print coords
+	def __init__(self, grid, coords):
+		self.sprites = pygame.sprite.Group()
+		for i in range(len(grid)):
+			self.sprites.add(opaque_grid(pygame.Rect(grid[i][0]*SIZE, grid[i][1]*SIZE, SIZE, SIZE)))
+
+	def get_sprites(self):
+		return self.sprites
+
+class background_map:
 	# Map class to create the background layer, holds any static and dynamical elements in the field.
 	
 	def __init__(self, map, height, width):
-		self.sprite = [] 
                 self.width = width
                 self.height = height
 		self.map = map
-		self.map_sprites = pygame.sprite.Group()
+		self.sprites = pygame.sprite.Group()
 		for x in range(self.width):
 			for y in range(self.height):
 				if map[x][y] == 'w':
 					print "Water at: (" + str(x) + "," + str(y) + ")"
-					self.map_sprites.add(Water(pygame.Rect(y*SIZE, x*SIZE, SIZE, SIZE)))
+					self.sprites.add(water(pygame.Rect(y*SIZE, x*SIZE, SIZE, SIZE)))
 				elif map[x][y] == 'l':
 					print "Land at: (" + str(x) + "," + str(y) + ")"
-					self.map_sprites.add(Land(pygame.Rect(y*SIZE, x*SIZE, SIZE, SIZE)))
-	def getSprites(self):
-		return self.map_sprites
+					self.sprites.add(land(pygame.Rect(y*SIZE, x*SIZE, SIZE, SIZE)))
+	def get_sprites(self):
+		return self.sprites
 
-class Player:
+class player:
 	# Class to create player or team in the game. One player may have many characters.
 
 	def __init__(self, coords):
-		self.sprite = [] 
 		self.coords = coords
-		self.player_sprites = pygame.sprite.Group()
-		self.player_characters = []
+		self.sprites = pygame.sprite.Group()
+		self.characters = []
 		for i in range(len(coords)):
 			if coords[i][0] == 'b':
 				x = coords[i][1]
 				y = coords[i][2]
 				print "Player1, ball sprite at: (" + str(x) + "," + str(y) + ")"
-				self.player_sprites.add(Water(pygame.Rect(y*SIZE, x*SIZE, SIZE, SIZE)))
-				self.player_characters.append([Character(2, [x, y], 90)])
-	def getSprites(self):
-		return self.map_sprites
+				self.sprites.add(ball(pygame.Rect(y*SIZE, x*SIZE, SIZE, SIZE)))
+				self.characters.append(character(2, [y, x], 90))
+	def get_sprites(self):
+		return self.sprites
+	
+	def get_characters(self):
+		return self.characters
 
-class Character:
+	def select_character(self, character):
+		self.characters[character].select()
+
+	def unselect_character(self, character):
+		self.characters[character].unselect()
+
+	def character_is_selected(self, character):
+		return self.characters[character].is_selected()
+	
+	def update_sprites(self):
+		self.sprites = pygame.sprite.Group()
+		for i in range(len(self.characters)):
+			coords = self.characters[i].get_coords()
+			self.sprites.add(ball(pygame.Rect(coords[0]*SIZE, coords[1]*SIZE, SIZE, SIZE)))
+
+class character:
 	# Universal class for any character in the game
 
 	def __init__(self, movement, coords, heading):
 		self.movement = movement	# Movement points left
 		self.coords = coords 		# Array of x and y
 		self.heading = heading		# Angle from north in degrees, possible values are: 0, 45, 90, 135, 180, 225, 270 and 315
+		self.selected = False
 	
-	def getCoords():			# Returns coordinates of the character, return is array [x, y]
+	def get_coords(self):			# Returns coordinates of the character, return is array [x, y]
 		return self.coords
 	
-	def setCoords(coords):			# Sets coordinates of characte, input is array of [x, y]
+	def set_coords(self, coords):			# Sets coordinates of characte, input is array of [x, y]
 		self.coords = coords
 
-	def getHeading():			# Returns heading of character in degrees
+	def get_heading(self):			# Returns heading of character in degrees
 		return self.heading
 
-	def setHeading(angle):			# Sets the absolute heading of character
+	def set_heading(self, angle):			# Sets the absolute heading of character
 		self.heading = angle
-
-	def turn(angle):			# Turns character given amount, relative to previous heading. For now only turns 90-degrees at a time
+	
+	def is_selected(self):
+		return self.selected
+	
+	def select(self):
+		self.selected = True
+	
+	def unselect(self):
+		self.selected = False
+	
+	def turn(self, angle):			# Turns character given amount, relative to previous heading. For now only turns 90-degrees at a time
 		angle = angle % 360
 		if angle <= 45:
 			self.heading = (self.heading + angle) % 360
@@ -122,7 +192,7 @@ class Character:
 		elif angle <= 135:
 			self.heading = (self.heading + angle) % 360
 
-	def moveForward(steps):		# Moves to headed direction given amount of steps
+	def move_forward(self, steps):		# Moves to headed direction given amount of steps
 		if self.movement <= steps:
 			if self.heading == 0:
 				self.coords[1] -= steps
@@ -133,12 +203,49 @@ class Character:
 			elif self.heading == 270:
 				self.coords[0] -= steps
 	
-	def getMovementGrid():			# Return grid of available cells to move to
-		 
+	def get_movement_grid(self):			# Return grid of available cells to move to		
+		#movement_grid = self.get_surroundings(self.coords)
+		movement_grid = []
+		if self.movement > 1:
+			for i in range(1, (self.movement + 1)):
+				"""
+				for i in range(len(movement_grid)):
+					temp_grid = self.get_surroundings(movement_grid[i])
+					movement_grid.extend(temp_grid)
+				"""
+				movement_grid.append([self.coords[0] + i, self.coords[1]])
+				movement_grid.append([self.coords[0] + i, self.coords[1] + i])
+				movement_grid.append([self.coords[0], self.coords[1] + i])
+				movement_grid.append([self.coords[0] - i, self.coords[1] + i])
+				movement_grid.append([self.coords[0] - i, self.coords[1]])
+				movement_grid.append([self.coords[0] - i, self.coords[1] - i])
+				movement_grid.append([self.coords[0] + i, self.coords[1] - i])
+				movement_grid.append([self.coords[0], self.coords[1] - i])
+		for k in range(len(movement_grid)):
+			#for j in range(len(movementGrid[k])):
+			print movement_grid[k]
+		return movement_grid
+
+	def get_surroundings(self, coords):
+		return_grid = []
+		print coords
+		for x in range(0, 3):
+			for y in range(0, 3):
+				if (coords[0] - x) >= 0:
+					if (coords[1] - y) >= 0:
+						return_grid.append([x,y])
+		return return_grid	
+
+	def is_legal_move(self, coords):
+		movement_grid = self.get_movement_grid()
+		for i in range(len(movement_grid)):
+			if coords == movement_grid[i]:
+				return True
+		return False
 
 # Following classes define the graphical elements, or Sprites.
 
-class Land(pygame.sprite.Sprite):
+class land(pygame.sprite.Sprite):
         
     def __init__(self, rect=None):
         pygame.sprite.Sprite.__init__(self) 
@@ -146,7 +253,7 @@ class Land(pygame.sprite.Sprite):
         if rect != None:
             self.rect = rect
 
-class Water(pygame.sprite.Sprite):
+class water(pygame.sprite.Sprite):
 
     def __init__(self, rect=None):
         pygame.sprite.Sprite.__init__(self)
@@ -154,7 +261,7 @@ class Water(pygame.sprite.Sprite):
         if rect != None:
             self.rect = rect
 
-class Ball(pygame.sprite.Sprite):
+class ball(pygame.sprite.Sprite):
 
    def __init__(self, rect=None):
 	pygame.sprite.Sprite.__init__(self)
@@ -162,8 +269,16 @@ class Ball(pygame.sprite.Sprite):
         if rect != None:
             self.rect = rect
 
+class opaque_grid(pygame.sprite.Sprite):
+
+   def __init__(self, rect=None):
+	pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('grid.png', None, SIZE/24)
+        if rect != None:
+            self.rect = rect
+
 # Main starts here
 
 if __name__ == "__main__":
-	MainWindow = PyManMain()
-	MainWindow.MainLoop()
+	main_window = xadir_main()
+	main_window.main_loop()
