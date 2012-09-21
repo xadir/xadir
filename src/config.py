4d4da75f712250df875489bc5dc73127da100bc8
Aleksi Torhamo <aleksi@torhamo.net>
2012-09-22 01:57:24 +0300

    Fixed BASEDIR calculation when importing from python shell
    Added MAPDIR

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import os
 
-BASEDIR = os.path.dirname(os.path.dirname(__file__))
+BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 GFXDIR = os.path.join(BASEDIR, 'gfx')
 SNDDIR = os.path.join(BASEDIR, 'snd')
+MAPDIR = os.path.join(BASEDIR, 'map')
 
