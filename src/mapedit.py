27eb8f57e1b34b94e643b950965d44f66d84cb48
Aleksi Torhamo <aleksi@torhamo.net>
2012-09-26 22:31:24 +0300

    Press space to fill in unambiguous tiles

--- src/mapedit.py
+++ src/mapedit.py
@@ -10,6 +10,13 @@ if not pygame.font:
 if not pygame.mixer:
 	print "Warning: Audio not enabled"
 
+TL, TR, BR, BL = range(4)
+def parse_tile(s):
+	if s is None:
+		return [None, None, None, None, None]
+	tl, tr, br, bl, n = s
+	return [tl, tr, br, bl, n]
+
 class MapEditor:
 	def __init__(self, width=640, height=480):
 		pygame.init()
@@ -54,27 +61,40 @@ class MapEditor:
 
 			for event in pygame.event.get():
 				if event.type == pygame.MOUSEBUTTONDOWN:
-					if left.contains(*event.pos):
-						area = 'left'
-					elif right.contains(*event.pos):
-						area = 'right'
-					start = event.pos
+					if event.button == 1:
+						if left.contains(*event.pos):
+							area = 'left'
+						elif right.contains(*event.pos):
+							area = 'right'
+						start = event.pos
+					else:
+						if right.contains(*event.pos):
+							x, y = right.translate(*event.pos)
+							x, y = x/17, y/17
+							inpaint(self.grid, self.tiles.keys(), (x, y))
 				elif event.type == pygame.MOUSEBUTTONUP:
-					if area == 'left' and left.contains(*event.pos):
-						x, y = left.translate(*event.pos)
-						x, y = x/17, y/17
-						tool = self.tools[x, y]
-					if area == 'right' and right.contains(*event.pos):
-						x, y = right.translate(*event.pos)
-						x, y = x/17, y/17
-						self.grid[x, y] = tool
-					area = None
-					start = None
+					if event.button == 1:
+						if area == 'left' and left.contains(*event.pos):
+							x, y = left.translate(*event.pos)
+							x, y = x/17, y/17
+							tool = self.tools[x, y]
+						if area == 'right' and right.contains(*event.pos):
+							x, y = right.translate(*event.pos)
+							x, y = x/17, y/17
+							self.grid[x, y] = tool
+						area = None
+						start = None
 				elif event.type == pygame.MOUSEMOTION:
 					if area == 'right' and right.contains(*event.pos):
 						x, y = right.translate(*event.pos)
 						x, y = x/17, y/17
 						self.grid[x, y] = tool
+				elif event.type == pygame.KEYUP:
+					if event.key == pygame.K_SPACE:
+						for pos, value in self.grid.items():
+							if value is not None:
+								continue
+							inpaint(self.grid, self.tiles.keys(), pos)
 				elif event.type == pygame.QUIT:
 					print 'SIZE', self.grid.width, self.grid.height
 					print
@@ -85,6 +105,31 @@ class MapEditor:
 					sys.exit()
 
 			time.sleep(0.05)
+
+def inpaint(grid, tile_names, pos):
+	items = list(grid.env_values(pos, 1))
+	if len(items) != 9:
+		return
+	items = map(parse_tile, items)
+	tl = list(set([items[3][TR], items[0][BR], items[1][BL]]) - set([None]))
+	tr = list(set([items[1][BR], items[2][BL], items[5][TL]]) - set([None]))
+	bl = list(set([items[3][BR], items[6][TR], items[7][TL]]) - set([None]))
+	br = list(set([items[7][TR], items[8][TL], items[5][BL]]) - set([None]))
+	tl = tl[0] if len(tl) == 1 else None
+	tr = tr[0] if len(tr) == 1 else None
+	bl = bl[0] if len(bl) == 1 else None
+	br = br[0] if len(br) == 1 else None
+	matches = set()
+	for name in tile_names:
+		ttl, ttr, tbr, tbl = parse_tile(name)[:4]
+		if tl in (ttl, None) and tr in (ttr, None) and bl in (tbl, None) and br in (tbr, None):
+			matches.add(ttl + ttr + tbr + tbl)
+	if len(matches) == 1:
+		try:
+			tn = list(set(item[-1] for item in items if item[-1] is not None))[0]
+		except IndexError:
+			tn = '1'
+		grid[pos] = list(matches)[0] + tn
 
 class UIComponent:
 	def __init__(self, x, y, width, height):
