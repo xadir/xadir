f861fc8c8852274c114c8a436d9628a6428c0e66
Heikki Juva <juva.hj@gmail.com>
2012-11-14 15:02:00 +0200

    Better preview and added some modularity

--- src/chartest.py
+++ src/chartest.py
@@ -13,6 +13,7 @@ if not pygame.mixer:
 	print "Warning: Audio not enabled"
 
 class CharTest:
+
 	def __init__(self):
 		pygame.init()
 		self.screen = pygame.display.set_mode((600, 600))
@@ -20,55 +21,99 @@ class CharTest:
 		self.charfield = pygame.Surface((600, 600))
 		self.charfield_rect = pygame.Rect(0, 0, 600, 600)
 		self.charfield.fill((150,150,150))
-		"""
-		self.load_characters('sprite_collection.png')
-		print self.charnames
-
-		self.sprites = pygame.sprite.LayeredUpdates()
-		tile = self.chartypes[self.charnames[0] + '_' + '270']
-		self.sprites.add(Tile(tile, pygame.Rect(TILE_SIZE[0], TILE_SIZE[1], *TILE_SIZE), layer = 0))
-		"""
-
-		self.sprites = pygame.sprite.LayeredUpdates()
-		self.sprites.add(race_sprite('Longear').get_sprite(0, 0, '270'))
-		self.sprites.add(race_sprite('Ghost').get_sprite(0, 60, '270'))
-		self.sprites.add(race_sprite('Croco').get_sprite(0, 120, '270'))
-		self.sprites.add(race_sprite('Longear').get_sprites(0, 200))
-		self.sprites.add(race_sprite('Ghost').get_sprites(0, 260))
-		self.sprites.add(race_sprite('Croco').get_sprites(0, 320))
 		
-		self.preview_sprite = race_sprite('Longear').get_sprite(0, 400, '270')
-		self.preview_sprite2 = race_sprite('Ghost').get_sprite(100, 400, '270')
-		self.preview_info = [('Health', 100), ('Attack', 100), ('Defence', 50)]
-		self.preview_info2 = [('Health', 100), ('Attack', 100), ('Defence', 50), ('Dexterity', 20)]
-		
-		self.preview = race_preview(self.preview_sprite, self.preview_info, 0, 400, (0, 0, 0), (150, 150, 150))
-		self.preview2 = race_preview(self.preview_sprite2, self.preview_info2, 100, 400, (0, 0, 0), (150, 150, 150))
+		self.preview = []
+		self.preview.append(race_preview(race_name = 'Longear', x = 0, y = 0))
+		self.preview.append(race_preview(race_name = 'Longear', x = 100, y = 0))
+		self.preview.append(race_preview(race_name = 'Croco', x = 200, y = 0))
 
 
 	def loop(self):
 
 		while 1:
 			self.screen.fill((127, 127, 127))
-			self.sprites.draw(self.screen)
-			self.preview.draw_preview(self.screen)
-			self.preview2.draw_preview(self.screen)
+			for i in range(len(self.preview)):
+				self.preview[i].draw(self.screen)
 			pygame.display.flip()
 
 			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
+				if event.type == pygame.MOUSEBUTTONDOWN:
+					if event.button == 1:
+						for p in self.preview:
+							if p.contains(*event.pos):
+								p.select()
+				elif event.type == pygame.QUIT:
 					sys.exit()
 
 			time.sleep(0.05)
 			
 class race_preview:
-	def __init__(self, sprite, text, x=0, y=0, font_color=(0,0,0), font_bg=(159, 182, 205)):
+	def __init__(self, race_name, sprite_file='race_sprites.txt', stat_file='race_stats.txt', x=0, y=0, font_color=(0,0,0), font_bg=(159, 182, 205), border_color=(50,50,50), border_selected=(50,50,0)):
+		self.race_name = race_name
+		self.sprite_file = sprite_file
+		self.stat_file = stat_file
+		self.x = x
+		self.y = y
+		self.font_color = font_color
+		self.font_bg = font_bg
+		self.border_color = border_color
+		self.border_selected = border_selected
+		self.load_sprite()
+		self.load_text()
+		self.load_box()
+	
+	def contains(self, x, y):
+		return self.box.contains(x, y)
+		
+	def select(self):
+		self.box.select()
+		
+	def unselect(self):
+		self.box.unselect()
+		
+	def toggle(self):
+		self.box.toggle()
+		
+	def load_sprite(self):
+		self.race_sprite = race_sprite(self.race_name).get_sprite(self.x, self.y, '270')
+	
+	def load_text(self):
+		path = os.path.join(GFXDIR, self.stat_file)
+		f = open(path, 'r')
+		
+		self.race_stats = {}
+		# iterate over the lines in the file
+		for line in f:
+			# split the line into a list of column values
+			columns = line.split(',')
+			# clean any whitespace off the items
+			columns = [col.strip() for col in columns]
+			stats = []
+			for i in range(1,len(columns)):
+				temp = columns[i].split('=')
+				temp = [col.strip() for col in temp]
+				stats.append(temp)
+			self.race_stats[columns[0]] = stats
+			
+		f.close()
+		
+	def load_box(self):
+		self.box = info_box(self.race_sprite, self.race_stats[self.race_name], self.x, self.y, self.font_color, self.font_bg, self.border_color, self.border_selected)
+	
+	def draw(self, surface):
+		self.box.draw_box(surface)
+		
+class info_box:
+	def __init__(self, sprite, text, x=0, y=0, font_color=(0,0,0), font_bg=(159, 182, 205), border_color_unselected=(50,50,50), border_color_selected=(255,255,255)):
 		self.sprite = sprite
 		self.text = text
 		self.x = x
 		self.y = y
 		self.font_color = font_color
 		self.font_bg = font_bg
+		self.border_color_unselected = border_color_unselected
+		self.border_color_selected = border_color_selected
+		self.border_color = border_color_selected
 		
 		self.sprites = pygame.sprite.LayeredUpdates()
 		self.sprites.add(self.sprite)
@@ -91,7 +136,7 @@ class race_preview:
 			self.inforect.append(self.temprect)
 
 		self.border = pygame.Surface((self.max_width + 8, (len(self.text))*15 + 69))
-		self.border.fill((50,50,50))
+		self.border.fill(self.border_color)
 		self.borderrect = self.border.get_rect()
 		self.borderrect.centerx = self.x + 70
 		self.borderrect.top = self.y + 48
@@ -102,7 +147,24 @@ class race_preview:
 		self.bgrect.centerx = self.x + 70
 		self.bgrect.top = self.y + 50
 		
-	def draw_preview(self, screen):
+	def contains(self, x, y):
+		return x >= self.x and y >= self.y and x < self.x + self.borderrect.width and y < self.y + self.borderrect.height
+		
+	def select(self):
+		print "selected"
+		self.border.fill(self.border_color_selected)
+		
+	def unselect(self):
+		print "unselected"
+		self.border.fill(self.border_color_unselected)
+		
+	def toggle(self):
+		if self.border_color == self.border_color_selected:
+			self.border.fill(self.border_color_unselected)
+		else: 
+			self.border.fill(self.border_color_selected)
+		
+	def draw_box(self, screen):
 		screen.blit(self.border, self.borderrect)
 		screen.blit(self.background, self.bgrect)
 		self.sprites.draw(screen)
@@ -110,8 +172,8 @@ class race_preview:
 			screen.blit(self.infotext[i], self.inforect[i])
 
 class race_sprite:
-	def load_races(self, conf_file):
-		path = os.path.join(GFXDIR, 'race_sprites.txt')
+	def load_races(self, race_sprite_file):
+		path = os.path.join(GFXDIR, race_sprite_file)
 		f = open(path, 'r')
 		
 		self.races = {}
@@ -122,6 +184,8 @@ class race_sprite:
 			# clean any whitespace off the items
 			columns = [col.strip() for col in columns]
 			self.races[columns[0]] = [columns[1], columns[2]]
+			
+		f.close()
 
 	def load_characters(self, filename):
 		#chars1 = load_tiles('sprite_collection.png', CHAR_SIZE, (255, 0, 255), SCALE)
@@ -161,6 +225,7 @@ class race_sprite:
 
 	def get_sprite(self, x=0, y=0, orientation=270):
 		self.sprites = pygame.sprite.LayeredUpdates()
+		print TILE_SIZE
 		self.sprites.add(Tile(self.chartypes[self.place + '_' + str(orientation)], pygame.Rect(x+TILE_SIZE[0], y+TILE_SIZE[1], *TILE_SIZE), layer = 0))
 		return self.sprites
 
