9d4e302d8603a4dd85cce5eaf399aaa2c3a5ace5
Zokol <juva.hj@gmail.com>
2012-12-19 21:26:13 +0200

    Fixed host ip issue

--- src/mapselection.py
+++ src/mapselection.py
@@ -59,10 +59,12 @@ class MapSelection:
 		self.port_input = eztext.Input(x=self.sidebar_container.x + 16, y=78, maxlength=4, color=COLOR_FONT, prompt='Port: ')
 
 		self.play_btn = FuncButton(self.sidebar_container, 10, 240, 218, 30, [["Play map", None]], None, ICON_FONTSIZE, self.screen, 1, (self.start_game, self.mapname), True, False, True)
-		if self.network_host:
-			self.ip_btn = FuncButton(self.sidebar_container, 10, 10, 218, 30, [["127.0.0.1", None]], None, ICON_FONTSIZE, self.screen, 1, (self.select_field, "ip"), True, False, True)
-		else:
-			self.ip_btn = FuncButton(self.sidebar_container, 10, 10, 218, 30, None, None, ICON_FONTSIZE, self.screen, 1, (self.select_field, "ip"), True, False, True)
+		#if self.network_host:
+		#	self.ip_btn = FuncButton(self.sidebar_container, 10, 10, 218, 30, [["127.0.0.1", None]], None, ICON_FONTSIZE, self.screen, 1, (self.select_field, "ip"), True, False, True)
+		#else:
+		self.ip_btn = FuncButton(self.sidebar_container, 10, 10, 218, 30, None, None, ICON_FONTSIZE, self.screen, 1, (self.select_field, "ip"), True, False, True)
+		if network_host:
+			self.ip_input.value += "127.0.0.1"
 		self.port_btn = FuncButton(self.sidebar_container, 10, 75, 218, 30, None, None, ICON_FONTSIZE, self.screen, 1, (self.select_field, "port"), True, False, True)
 
 		self.buttons = []
@@ -110,9 +112,10 @@ class MapSelection:
 	def select_field(self, field):
 		print "Clicked on field button"
 		if field == "ip":
-			print "Selected IP field"
-			self.ip_input_enabled = True
-			self.port_input_enabled = False
+			if not self.network_host:
+				print "Selected IP field"
+				self.ip_input_enabled = True
+				self.port_input_enabled = False
 		if field == "port":
 			print "Selected Port field"
 			self.ip_input_enabled = False
@@ -153,8 +156,8 @@ class MapSelection:
 		print "Map: ", mapname
 		print "Network game: ", self.network
 		print "Hosting: ", self.network_host
-		print "Selected IP: ", self.ip_field.value
-		print "Selected Port: ", self.port_field.value
+		print "Selected IP: ", self.ip_input.value
+		print "Selected Port: ", self.port_input.value
 
 	def do_done(self):
 		self.done = True
@@ -201,8 +204,8 @@ class MapSelection:
 				self.port_input.update(events)
 
 			# blit txtbx on the sceen
-			if not self.network_host:
-				self.ip_input.draw(self.screen)
+			#if not self.network_host:
+			self.ip_input.draw(self.screen)
 			self.port_input.draw(self.screen)
 
 			pygame.display.flip()
@@ -223,6 +226,6 @@ if __name__ == "__main__":
 		mapname = sys.argv[1]
 
 	screen = init_pygame()
-	win = MapSelection(screen, "map_new.txt", True, False)
+	win = MapSelection(screen, "map_new.txt", True, True)
 	win.loop()
 
