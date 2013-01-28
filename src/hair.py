1754dd0d01e007f4bcea97ec90abd2a0de4cbde5
Aleksi Torhamo <aleksi@torhamo.net>
2013-01-28 21:48:12 +0200

    Added "correct" values for hair fixups and race hairlines

--- src/hair.py
+++ src/hair.py
@@ -1,11 +1,25 @@
 
+hair_data = [
+	('a', 0, 0),
+	('b', 0, 0),
+	('c', 0, -2),
+	('d', 0, -1),
+	('e', 0, 0),
+	('f', 0, 0),
+	('g', 0, -1),
+	('h', 1, 0),
+	('i', 0, -3),
+	('j', 1, -2),
+]
+
 class Hair:
-	def __init__(self, name):
+	def __init__(self, name, xoffset = 0, yoffset = 0):
 		self.name = name
-		self.xoffset = self.yoffset = 0
+		self.xoffset = xoffset
+		self.yoffset = yoffset
 
 hairs = {}
 
-for name in 'abcdefghij':
-	hairs[name] = Hair(name)
+for name, xoff, yoff in hair_data:
+	hairs[name] = Hair(name, xoff, yoff)
 
