3912df4a01e36e424ecc1a46880a0798c991c30e
Zokol <juva.hj@gmail.com>
2013-02-10 23:26:53 +0200

    Added print to armors

--- src/armor.py
+++ src/armor.py
@@ -42,6 +42,12 @@ class Armor:
 		enchanted_damage_reduction_type = set([random.choice(cls.damage_types)])
 		return cls(name, miss_chance, damage_reduction, enchanted_damage_reduction, enchanted_damage_reduction_type, 10)
 
+	def __repr__(self):
+		return 'Armor(%r, %r, %r, %r)' % (self.name, self.miss_chance, self.damage_reduction, self.enchanted_damage_reduction)
+
+	def __str__(self):
+		return '%s (miss:%d dmg:%d ench_dmg:%d%s)' % (self.name or 'Armor', self.miss_chance, self.damage_reduction, self.enchanted_damage_reduction, '/'.join(self.enchanted_damage_reduction_type))
+
 armors = {}
 for name, type, miss_chance, resilience, dmg_reduct, price in armor_data:
 	armors[name] = Armor(name, miss_chance, dmg_reduct, 0, set(), price)
