74b0dc58a04443402254807c29a81f657daa13d3
Zokol <juva.hj@gmail.com>
2013-02-11 14:51:18 +0200

    New class for player in multi-round games

---
+++ src/player.py
@@ -0,0 +1,9 @@
+class Player:
+
+	def __init__(self, name, characters, inventory, money):
+		self.name = name
+		self.characters = characters
+		self.inventory = inventory
+		self.money = money
+		self.rounds_played = 0
+		self.rounds_won = 0
