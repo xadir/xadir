9dc9724050c712a2e2fa169525a5d0f8e35fade5
Aleksi Torhamo <aleksi@torhamo.net>
2013-03-04 00:07:14 +0200

    Revert "Tried to fix textfield updating, and failed"
    This reverts commit bcd7bfa0f027dd3a1764ac75b511460edcaeb332.
    The problem this commit tried to fix was fixed in the last commit.

--- src/eztext.py
+++ src/eztext.py
@@ -47,12 +47,10 @@ class Input:
 
     def update(self, events):
         """ Update the input based on passed events """
-	print "Possible keypress"
-	contains = True
         for event in events:
             if event.type == KEYUP:
                 if event.key == K_LSHIFT or event.key == K_RSHIFT: self.shifted = False
-            elif event.type == KEYDOWN:
+            if event.type == KEYDOWN:
                 if event.key == K_BACKSPACE: self.value = self.value[:-1]
                 elif event.key == K_LSHIFT or event.key == K_RSHIFT: self.shifted = True
                 elif event.key == K_SPACE: self.value += ' '
@@ -153,7 +151,4 @@ class Input:
                     elif event.key == K_PERIOD and '>' in self.restricted: self.value += '>'
                     elif event.key == K_SLASH and '?' in self.restricted: self.value += '?'
 
-        	else: contains = False
-	if len(self.value) > self.maxlength and self.maxlength >= 0: self.value = self.value[:-1]
-	print contains
-	return contains
+        if len(self.value) > self.maxlength and self.maxlength >= 0: self.value = self.value[:-1]
