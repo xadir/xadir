fc2f963d9352b4a720bacc7a75f8d4159288e6ed
Aleksi Torhamo <aleksi@torhamo.net>
2013-03-06 11:22:29 +0200

    Some final cosmetic changes on extext

--- src/eztext.py
+++ src/eztext.py
@@ -6,10 +6,9 @@ from config import FONT, FONTSCALE
 DEFAULT_ALLOWED = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~ '
 
 class Input:
-	""" A text input for pygame apps """
-	def __init__(self, x = 0, y = 0, font = None, color = (0, 0, 0), restricted = DEFAULT_ALLOWED, maxlength = -1, prompt = '', handle_enter = None):
-		self.x = x
-		self.y = y
+	"""A text input for pygame apps"""
+	def __init__(self, x = 0, y = 0, font = None, color = (0, 0, 0), restricted = DEFAULT_ALLOWED, maxlength = None, prompt = '', handle_enter = None):
+		self.pos = (x, y)
 		self.font = font or pygame.font.Font(FONT, int(32*FONTSCALE))
 		self.color = color
 		self.restricted = restricted
@@ -19,29 +18,32 @@ class Input:
 		self._handle_enter = handle_enter
 
 	def set_pos(self, x, y):
-		""" Set the position to x, y """
-		self.x = x
-		self.y = y
+		"""Set the position to x, y"""
+		self.pos = (x, y)
 
 	def set_font(self, font):
-		""" Set the font for the input """
+		"""Set the font for the input"""
 		self.font = font
 
 	def draw(self, surface):
-		""" Draw the text input to a surface """
-		text = self.font.render(self.prompt+self.value, 1, self.color)
-		surface.blit(text, (self.x, self.y))
+		"""Draw the text input to a surface"""
+		text = self.font.render(self.prompt + self.value, 1, self.color)
+		surface.blit(text, self.pos)
 
 	def handle_enter(self):
 		if self._handle_enter:
 			self._handle_enter()
 
 	def update(self, events):
-		""" Update the input based on passed events """
+		"""Update the input based on passed events"""
 		for event in events:
 			if event.type == KEYDOWN:
-				if event.key == K_BACKSPACE: self.value = self.value[:-1]
-				elif event.key == K_RETURN: self.handle_enter()
-				elif event.unicode in self.restricted: self.value += event.unicode
-		if len(self.value) > self.maxlength and self.maxlength >= 0: self.value = self.value[:-1]
+				if event.key == K_BACKSPACE:
+					self.value = self.value[:-1]
+				elif event.key == K_RETURN:
+					self.handle_enter()
+				elif event.unicode in self.restricted:
+					self.value += event.unicode
+		if self.maxlength is not None and len(self.value) > self.maxlength:
+			self.value = self.value[:self.maxlength]
 
