b49d0676be0ae0f0d30bfb6f281f4bf9d9da9aea
Aleksi Torhamo <aleksi@torhamo.net>
2013-03-06 23:29:57 +0200

    Moved event handling from update() to event()

--- src/eztext.py
+++ src/eztext.py
@@ -6,7 +6,6 @@ from config import FONT, FONTSCALE
 DEFAULT_ALLOWED = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~ '
 
 class Input:
-	"""A text input for pygame apps"""
 	def __init__(self, x = 0, y = 0, font = None, color = (0, 0, 0), restricted = DEFAULT_ALLOWED, maxlength = None, prompt = '', handle_enter = None):
 		self.pos = (x, y)
 		self.font = font or pygame.font.Font(FONT, int(32*FONTSCALE))
@@ -18,7 +17,6 @@ class Input:
 		self._handle_enter = handle_enter
 
 	def draw(self, surface):
-		"""Draw the text input to a surface"""
 		text = self.font.render(self.prompt + self.value, 1, self.color)
 		surface.blit(text, self.pos)
 
@@ -27,15 +25,17 @@ class Input:
 			self._handle_enter()
 
 	def update(self, events):
-		"""Update the input based on passed events"""
 		for event in events:
-			if event.type == KEYDOWN:
-				if event.key == K_BACKSPACE:
-					self.value = self.value[:-1]
-				elif event.key == K_RETURN:
-					self.handle_enter()
-				elif event.unicode in self.restricted:
-					self.value += event.unicode
+			self.event(event)
+
+	def event(self, event):
+		if event.type == KEYDOWN:
+			if event.key == K_BACKSPACE:
+				self.value = self.value[:-1]
+			elif event.key == K_RETURN:
+				self.handle_enter()
+			elif event.unicode in self.restricted:
+				self.value += event.unicode
 		if self.maxlength is not None and len(self.value) > self.maxlength:
 			self.value = self.value[:self.maxlength]
 
