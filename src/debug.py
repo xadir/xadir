f6f7a45b1cc165369977fb2fe0c49c549a079b64
Aleksi Torhamo <aleksi@torhamo.net>
2013-03-11 23:57:12 +0200

    Updated to latest version of extexc

--- src/debug.py
+++ src/debug.py
@@ -1,5 +1,5 @@
 import traceback, types, sys
-import linecache
+import linecache, dis
 
 def saferepr(obj):
 	try:
@@ -22,6 +22,44 @@ def saferepr(obj):
 
 		return '<repr-failed-for %s at %#08x>' % (info, id(obj))
 
+def get_names_at(co, lasti):
+	"""Get the names that are referenced in the active part of code"""
+	code = co.co_code
+	labels = dis.findlabels(code)
+	linestarts = dict(dis.findlinestarts(co))
+	n = len(code)
+	i = 0
+	extended_arg = 0
+	free = None
+	linenames = []
+	found = False
+	while i < n:
+		c = code[i]
+		op = ord(c)
+		if i in linestarts:
+			if found:
+				return linenames
+			linenames = []
+		if i == lasti:
+			found = True
+		i = i+1
+		if op >= dis.HAVE_ARGUMENT:
+			oparg = ord(code[i]) + ord(code[i+1])*256 + extended_arg
+			extended_arg = 0
+			i = i+2
+			if op == dis.EXTENDED_ARG:
+				extended_arg = oparg*65536L
+			if op in dis.hasname:
+				linenames.append(co.co_names[oparg])
+			elif op in dis.haslocal:
+				linenames.append(co.co_varnames[oparg])
+			elif op in dis.hasfree:
+				if free is None:
+					free = co.co_cellvars + co.co_freevars
+				linenames.append(free[oparg])
+	if found:
+		return linenames
+
 def format_exc(exctype, value, tb):
 	info = ''.join(traceback.format_exception(exctype, value, tb))
 
@@ -29,19 +67,23 @@ def format_exc(exctype, value, tb):
 	while tb:
 		f = tb.tb_frame
 		co = f.f_code
+		active = get_names_at(co, f.f_lasti)
 		linecache.checkcache(co.co_filename)
 		line = linecache.getline(co.co_filename, tb.tb_lineno, f.f_globals)
-		item = '  File "%s", line %d, in %s\n' % (co.co_filename, tb.tb_lineno, co.co_name)
+		item = '  \x1b[1;32mFile "%s", line %d, in %s\x1b[m\n' % (co.co_filename, tb.tb_lineno, co.co_name)
 		if line:
 			item = item + '    %s\n' % line.strip()
 
 		locs = tb.tb_frame.f_locals
+		if locs == tb.tb_frame.f_globals:
+			locs = dict((key, locs[key]) for key in active)
+
 		try:
 			just = max(len(key) for key in locs.keys())
 		except:
 			just = 0
 
-		loc_info.append(item + '  Locals:\n' + '\n'.join("    %-*s: %s" % (just, key, saferepr(value)[:512]) for key, value in sorted(locs.iteritems())))
+		loc_info.append(item + '  \x1b[1;34mLocals:\x1b[m\n' + '\n'.join("    %s%-*s%s: %s" % (('\x1b[1;31m' if key in active else ''), just, key, '\x1b[m', saferepr(value)[:512]) for key, value in sorted(locs.iteritems(), key = lambda (k, v): (k not in active, k))))
 
 		tb = tb.tb_next
 
