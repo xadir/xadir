dcd23330ecb8705c77ab5178f614c82e974394fa
Aleksi Torhamo <aleksi@torhamo.net>
2013-03-13 10:47:28 +0200

    Display cursor on active Input field

--- src/eztext.py
+++ src/eztext.py
@@ -9,7 +9,7 @@ from config import FONT, FONTSCALE
 DEFAULT_ALLOWED = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~ '
 
 class Input(StateTrackingSprite, UIObject):
-	def __init__(self, parent, rel_pos, size, font = None, color = (0, 0, 0), restricted = DEFAULT_ALLOWED, maxlength = None, prompt = '', handle_enter = None):
+	def __init__(self, parent, rel_pos, size, font = None, color = (0, 0, 0), restricted = DEFAULT_ALLOWED, maxlength = None, prompt = '', handle_enter = None, interval = 10):
 		StateTrackingSprite.__init__(self)
 		UIObject.__init__(self, parent, rel_pos, size)
 
@@ -21,17 +21,25 @@ class Input(StateTrackingSprite, UIObject):
 		self.prompt = prompt
 		self.value = ''
 		self._handle_enter = handle_enter
+		self.interval = interval
+		self.count = 0
 
 	rect = property(lambda self: pygame.Rect(self.pos, self.image.get_size()))
 
+	def update(self):
+		self.count = (self.count + 1) % (2 * self.interval)
+		StateTrackingSprite.update(self)
+
 	def get_state(self):
-		return self.font, self.color, self.prompt, self.value
+		return self.font, self.color, self.prompt, self.value, (self.count / self.interval) % 2 == 0 and get_inputfocus() == self
 
 	def redraw(self):
-		font, color, prompt, value = self.state
+		font, color, prompt, value, cursor = self.state
 		text = font.render(prompt + value, True, color)
 		self.image.fill((255, 255, 255))
 		self.image.blit(text, (0, 0))
+		if cursor:
+			self.image.fill((0, 0, 0), (text.get_width() + 2, 2, 2, self.height - 4))
 
 	def handle_enter(self):
 		if self._handle_enter:
@@ -41,6 +49,7 @@ class Input(StateTrackingSprite, UIObject):
 		if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
 			if self.contains(*event.pos):
 				grab_inputfocus(self)
+				self.count = 0
 			else:
 				ungrab_inputfocus(self)
 		if event.type == KEYDOWN and get_inputfocus() == self:
@@ -50,6 +59,7 @@ class Input(StateTrackingSprite, UIObject):
 				self.handle_enter()
 			elif event.unicode in self.restricted:
 				self.value += event.unicode
+			self.count = 0
 		if self.maxlength is not None and len(self.value) > self.maxlength:
 			self.value = self.value[:self.maxlength]
 
